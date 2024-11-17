e7be51391c5d7a55587028d642ca434c4fe93c3f
grifondopala <grifondopala@gmail.com>
2024-11-17 23:36:42 +0300

    first commit

---
+++ lib.h
@@ -0,0 +1,51 @@
+#ifndef LIB_H 
+#define LIB_H
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <limits>
+#include <math.h>
+#include <numbers>
+
+using namespace cv;
+using namespace std;
+
+struct MyPoint {
+	double x;
+	double y;
+	double z;
+};
+
+struct MyPoint2d {
+	double x;
+	double y;
+};
+
+const Vec3b BLACK = { 0, 0, 0 };
+const Vec3b WHITE = { 255, 255, 255 };
+const Vec3b RED = { 0, 0, 255 };
+const Vec3b GREEN = { 0, 255, 0 };
+const Vec3b BLUE = { 255, 0, 0 };
+const Vec3b YELLOW = { 0, 255, 255 };
+
+void onePointTransform(vector<vector<MyPoint>> faces, double r, Mat& image);
+
+void XYProjection(vector<vector<MyPoint>> faces, Mat& image);
+
+void setPixel(int x, int y, Mat& image, Vec3b color);
+
+void fillBackground(Mat& image, Vec3b color);
+
+vector<vector<MyPoint>> resize(vector<vector<MyPoint>> faces, double M);
+
+vector<vector<MyPoint>> translate(vector<vector<MyPoint>> faces, double tx, double ty, double tz);
+
+vector<vector<MyPoint>> deleteFaces(vector<vector<MyPoint>> faces);
+
+vector<vector<MyPoint>> rotateFaces(vector<vector<MyPoint>> faces, MyPoint point, double phi);
+
+#endif
